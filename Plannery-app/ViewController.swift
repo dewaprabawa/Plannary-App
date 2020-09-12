cf3c7ca6be6d0776c8eccf464b0a156c0d92786f
dewa <dewaprabawa>
2020-09-12 18:51:06 +0800

    install app

---
+++ Plannery-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Plannery-app
+//
+//  Created by Dewa Prabawa on 12/09/20.
+//  Copyright © 2020 Dewa Prabawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
